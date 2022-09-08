760cef87007568d6974ebdc6d044fbbfc00bc8d4
Ruan Pablo <ruan_goio10@hotmail.com>
2022-09-08 00:27:15 -0300

    Fix: About ME responsive

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -10,28 +10,42 @@ export function About() {
 			<div className="mt-12 h-[360px] mx-8 bg-transparent flex">
 				<h2 className="text-red font-MPlus font-bold">3d HERE</h2>
 			</div>
-			<div className="mt-12 h-[360px] mx-8 flex justify-end">
-				<header className="flex gap-4 h-40">
-					<div className="flex flex-col">
-						<h2 className="font-nunito font-bold text-text text-right dark:text-white">Ruan Pablo Gomes Rocha</h2>
-						<span className="font-nunito font-medium text-subText text-right dark:text-secondary">Junior FrontEnd and Mobile Developer</span>
+			<div className="mt-12 h-[360px] mx-8 flex flex-col">
+				<header className="flex gap-4 justify-end">
 
-						<div className="flex mt-auto mb-4">
-							<span className="flex items-center justify-center px-4 py-1 rounded-lg text-lg font-nunito font-medium bg-white text-red dark:bg-darkSecondary transition-all duration-500">
-								<Icon
-									path={mdiInformationVariant}
-									size={1}
-								/>
-								About ME
-							</span>
-						</div>
+					<div className="flex flex-col">
+						<h2 className="font-nunito font-bold text-text text-right dark:text-white">
+							Ruan Pablo Gomes Rocha
+						</h2>
+						<span className="font-nunito font-medium text-subText text-right dark:text-secondary">
+							Junior FrontEnd and Mobile Developer
+						</span>
 					</div>
+
 					<img
 						className="w-[100px] h-[100px] rounded-full"
 						src="https://i.pinimg.com/564x/0b/02/d0/0b02d0f8910069b921ba9fb041ca27be.jpg"
 						alt="Perfil Image"
 					/>
+
 				</header>
+
+				<span className="flex w-[140px] mb-4 items-center justify-center px-4 py-1 rounded-lg text-lg font-nunito font-medium bg-white text-red dark:bg-darkSecondary transition-all duration-500">
+					<Icon
+						path={mdiInformationVariant}
+						size={1}
+					/>
+					About ME
+				</span>
+				<span className="text-justify font-nunito text-primary h-auto">
+					Lorem Ipsum is simply dummy text of the printing and
+					typesetting industry. Lorem Ipsum has been the industrys
+					standard dummy text ever since the 1500s, when an unknown printer
+					took a galley of type and scrambled it to make a type specimen book.
+					It has survived not only five centuries, but also the leap into electronic
+					typesetting, remaining essentially unchanged. It was popularised in the 1960s
+					with the release of Letraset sheets containing Lorem Ipsum passages.
+				</span>
 			</div>
 		</div>
 	);
