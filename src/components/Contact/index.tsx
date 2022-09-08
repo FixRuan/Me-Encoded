bfa8d2123d1f07232774321037ba3c2a6926d248
Ruan Pablo <ruan_goio10@hotmail.com>
2022-09-08 19:34:17 -0300

    Form Component

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -10,8 +10,8 @@ export function Contact() {
 		<section className="mb-4 px-8 ">
 			<h1 className="font-bold font-nunito text-DarkRed dark:text-red">Contact-me</h1>
 
-			<div className="grid md:grid-cols-2 mt-4">
-				<div className="bg-white p-8 rounded-md relative md:max-w-[350px] h-[400px] dark:bg-darkSecondary  transition-all duration-700">
+			<div className="flex flex-col md:flex-row mt-4 gap-4">
+				<div className="bg-white p-8 rounded-md relative md:max-w-[350px] h-[400px] dark:bg-darkSecondary  transition-all duration-700 mb-4">
 					<h2 className="text-text text-xl font-nunito font-medium dark:text-white">Contact Information</h2>
 					<p className="font-nunito text-md dark:text-primary">
 						Fill  up the form and I will get back to you within 24 hours
@@ -25,7 +25,7 @@ export function Contact() {
 							/>
 							(xx) xxxxx-xxxx
 						</span>
-						<span className="flex gap-1 items-center text-text font-nunito font-medium text-xl  dark:text-secondary">
+						<span className="flex mt-4 gap-1 items-center text-text font-nunito font-medium text-xl  dark:text-secondary">
 							<Icon
 								path={mdiEmail}
 								size={1}
@@ -56,7 +56,75 @@ export function Contact() {
 				</div>
 
 
-				<div></div>
+				<form className="flex flex-col flex-1 mb-8">
+
+					<div className="flex items-center gap-2 mb-8">
+						<div className="flex flex-col w-1/2">
+							<label htmlFor="name" className="text-red font-nunito font-medium">
+								First Name
+							</label>
+							<input
+								type="text"
+								id="name"
+								className="h-10 border-b-2 border-darkSecondary bg-transparent outline-none 
+								text-text dark:text-white dark:border-white"
+							/>
+						</div>
+
+						<div className="flex flex-col w-1/2">
+							<label htmlFor="lastName" className="text-red font-nunito font-medium">
+								Last Name
+							</label>
+							<input
+								type="text"
+								id="lastName"
+								className="h-10 border-b-2 border-darkSecondary bg-transparent outline-none 
+								text-text dark:text-white dark:border-white"
+							/>
+						</div>
+					</div>
+
+					<div className="flex items-center gap-2 mb-8">
+						<div className="flex flex-col w-1/2">
+							<label htmlFor="email" className="text-red font-nunito font-medium">
+								Email
+							</label>
+							<input
+								type="text"
+								id="email"
+								className="h-10 border-b-2 border-darkSecondary bg-transparent outline-none 
+								text-text dark:text-white dark:border-white"
+							/>
+						</div>
+
+						<div className="flex flex-col w-1/2">
+							<label htmlFor="phone" className="text-red font-nunito font-medium">
+								Phone
+							</label>
+							<input
+								type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+								className="h-10 border-b-2 border-darkSecondary bg-transparent outline-none 
+								text-text dark:text-white dark:border-white"
+							/>
+						</div>
+					</div>
+
+					<div className="flex">
+						<div className="flex flex-col w-full">
+							<label htmlFor="message" className="text-red font-nunito font-medium">
+								Message
+							</label>
+							<textarea
+								id="message"
+								className="h-10 border-b-2 border-darkSecondary bg-transparent outline-none 
+								text-text dark:text-white dark:border-white"
+								maxLength={10}
+								cols={1}
+								minLength={10}
+							/>
+						</div>
+					</div>
+				</form>
 			</div>
 		</section>
 	);
