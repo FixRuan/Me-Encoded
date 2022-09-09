6445f019a622476b1feebfbce45c0f961682a8e2
Ruan Pablo <ruan_goio10@hotmail.com>
2022-09-09 17:18:57 -0300

    add more projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -13,19 +13,19 @@ export function Projects() {
 			<div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4 mt-12">
 
 				{projectsArray.map(project => (
-					<div key={project.name} className="flex flex-col mb-4">
+					<a target="_blank" href={project.link} key={project.name} className="flex flex-col mb-4 group" rel="noreferrer">
 						<span className="text-center font-nunito font-bold dark:text-white">
 							{project.name}
 						</span>
 
 						<div className="flex p-2 items-center justify-center">
-							<img className="rounded-md" src={project.image} alt="project image" />
+							<img className="rounded-md group-hover:scale-105 transition-all duration-500" src={project.image} alt="project image" />
 						</div>
 
 						<p className="text-center md:text-base text-sm font-nunito font-medium dark:text-primary">
 							{project.description}
 						</p>
-					</div>
+					</a>
 				))}
 
 			</div>
--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -4,11 +4,14 @@ import RentxImage from "../assets/projects/rentx.png";
 import GoFinancesImage from "../assets/projects/gofinances.png";
 import PokedexImage from "../assets/projects/pokedex.png";
 import IgnewsImage from "../assets/projects/ignews.png";
+import GithubBlogImage from "../assets/projects/githubBlog.png";
+import RocketHelpImage from "../assets/projects/rocketHelp.png";
 
 interface Project {
 	name: string;
 	description: string;
 	image: string;
+	link: string;
 }
 
 const projectsArray: Project[] = [
@@ -16,31 +19,49 @@ const projectsArray: Project[] = [
 		name: "Devlândia",
 		description: "Aplicativo para aprender programação gratuito.",
 		image: DevlandiaImage,
+		link: "https://github.com/FixRuan/dev-landia"
 	},
 	{
 		name: "Go Pizza",
 		description: "Um aplicativo para gerir entregas de pizzas.",
 		image: GoPizzaImage,
+		link: "https://github.com/FixRuan/Go-Pizza"
 	},
 	{
 		name: "RentX",
 		description: "Aplicativo para aluguel de carros",
 		image: RentxImage,
+		link: "https://github.com/FixRuan/RentX"
 	},
 	{
 		name: "Go Finances",
 		description: "Aplicativo para gerir finanças desenvolvido na Rocketseat.",
 		image: GoFinancesImage,
+		link: "https://github.com/FixRuan/Go-Finances"
 	},
 	{
 		name: "Pokedex",
 		description: "Aplicativo para visualizar pokemons",
 		image: PokedexImage,
+		link: "https://github.com/FixRuan/Pokedex-challenge"
 	},
 	{
 		name: "IGnews",
 		description: "Blog com opções de pagamento com stripe",
 		image: IgnewsImage,
+		link: "https://github.com/FixRuan/ig.News"
+	},
+	{
+		name: "Github Blog",
+		description: "Blog feito com api do github, desafio da trilha de React (rocketseat)",
+		image: GithubBlogImage,
+		link: "https://github.com/FixRuan/Github-Blog"
+	},
+	{
+		name: "Rocket Help",
+		description: "Aplicatipo para resoluções de problemas, desenvolvido no Ignite Lab (rocketseat)",
+		image: RocketHelpImage,
+		link: "https://github.com/FixRuan/Rocket-Help"
 	},
 ];
 
